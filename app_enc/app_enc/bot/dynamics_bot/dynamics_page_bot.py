1b0332767aa112754f984dbbd9f911ebe82f783f
Robert Tolentino M. <robert.tolentino@terranovatrading.com.pe>
2024-05-13 13:00:40 -0500

    Improve bot speed al momento de aceptar actualizar lineas de pedido

--- app_enc/app_enc/bot/dynamics_bot/dynamics_page_bot.py
+++ app_enc/app_enc/bot/dynamics_bot/dynamics_page_bot.py
@@ -543,23 +543,37 @@ class Dynamics_Bot:
                 print('Click en vistas globales')
                 self._hacer_clic_xpath(self.xpath_button_vistas_globales)
 
+            # try:
+            #     print('Click en Vista formulario de pedido de venta')
+            #     self._esperar_n_segundos(1)
+            #     self._hacer_clic_xpath(self.xpath_vista_formulario_pedido_venta)
+            # except:
+            #     try:
+            #         self._wait_hide_div_bloking(15)
+            #         print('Click en Vista formulario de pedido de venta despues del bloking')
+            #         self._esperar_n_segundos(2)
+            #         self._hacer_clic_xpath(self.xpath_vista_formulario_pedido_venta)
+            #     except:
+            #         print('Click panel actualizar lineas de pedido')
+            #         self._hacer_clic_xpath(self.xpath_span_actualizar_lineas_pedido)
+            #         self._hacer_clic_xpath(self.xpath_button_aceptar_actualizar)
+            #         print('Click en Vista formulario de pedido de venta')
+            #         self._esperar_n_segundos(2)
+            #         self._hacer_clic_xpath(self.xpath_vista_formulario_pedido_venta)
+
+            self._wait_hide_div_bloking(10)
             try:
+                print('Click panel Actualizar lineas de pedido')
+                self._hacer_clic_xpath(self.xpath_span_actualizar_lineas_pedido)
+                self._hacer_clic_xpath(self.xpath_button_aceptar_actualizar)
+                self._esperar_n_segundos(10)
                 print('Click en Vista formulario de pedido de venta')
-                self._esperar_n_segundos(1)
                 self._hacer_clic_xpath(self.xpath_vista_formulario_pedido_venta)
             except:
-                try:
-                    self._wait_hide_div_bloking(15)
-                    print('Click en Vista formulario de pedido de venta despues del bloking')
-                    self._esperar_n_segundos(2)
-                    self._hacer_clic_xpath(self.xpath_vista_formulario_pedido_venta)
-                except:
-                    print('Click panel actualizar lineas de pedido')
-                    self._hacer_clic_xpath(self.xpath_span_actualizar_lineas_pedido)
-                    self._hacer_clic_xpath(self.xpath_button_aceptar_actualizar)
-                    print('Click en Vista formulario de pedido de venta')
-                    self._esperar_n_segundos(2)
-                    self._hacer_clic_xpath(self.xpath_vista_formulario_pedido_venta)
+                print('-- No se mostro Actualizar lineas de pedido')
+                self._esperar_n_segundos(2)
+                print('Click en Vista formulario de pedido de venta')
+                self._hacer_clic_xpath(self.xpath_vista_formulario_pedido_venta)
 
             print('Entro a la vista FORMULARIO DE PEDIDO DE VENTA')
             self._wait_hide_div_bloking(30)
